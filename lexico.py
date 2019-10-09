49d1fa19fa4d965387fa909891de34e31afd6db8
Mariane Leite <mariane.leite@topos.local>
2019-10-09 14:17:31 -0300

    Ajustes para reconhecer a expressao regular e criar os tokens

--- lexico.py
+++ lexico.py
@@ -14,7 +14,7 @@ def inputCodigo():
 
 #cria uma tabela com as palavras reservadas possiveis
 def definirPalavrasReservadas():
-  reservadas = [];
+  reservadas = []
   reservadas.append("main")
   reservadas.append("{")
   reservadas.append("}")
@@ -22,6 +22,7 @@ def definirPalavrasReservadas():
   reservadas.append(")")
   reservadas.append("if")
   reservadas.append("else")  
+  reservadas.append("for")
   reservadas.append("while")
   reservadas.append("int")
   reservadas.append("float")
@@ -33,11 +34,13 @@ def definirPalavrasReservadas():
   reservadas.append("-")
   reservadas.append("*")
   reservadas.append("/")
+  reservadas.append(";")
 
-  return reservadas;
+  return reservadas
 
+#cria uma tabela com as classes léxicas
 def definirClasses():
-  classes = [[]];
+  classes = []
   #Codigo, Classe Léxica, Expressão Regular
   classes.append([0, "main", "^main$"])
   classes.append([1, "if", "^if$"])
@@ -48,73 +51,72 @@ def definirClasses():
   classes.append([6, "atribuição", "^=$"])
   classes.append([7, "identificador", "^[a-zA-Z][a-zA-Z0-9]*$"])
   classes.append([8, "operador relacional", "^==$|^!=$|^<$|^<=$|^>$|^>=$"])
-  classes.append([9, "operador aritmético", "^+$|^-$|^/$|^*$"])
+  classes.append([9, "operador aritmético", "^[+]$|^[-]$|^[/]$|^[*]$"])
   classes.append([10, "operador lógico", "^and$|^or$"])
   classes.append([11, "inteiro", "^[-]?[0-9]+$"])
   classes.append([12, "float", "^[-]?[0-9]+([.][0-9]+)?$"])
-  #falta ler caracter especial
-  classes.append([13, "string", "^[\"][0-9a-zA-Z\\t\\n\\r ]*[\"]$"])
-  classes.append([14, "abrir parênteses", "("])
-  classes.append([15, "fechar parênteses", ")"])
-  classes.append([16, "abrir chaves", "{"])
-  classes.append([17, "fechar chaves", "}"])
-  classes.append([18, "delimitador de linha", ";"])
+  classes.append([13, "string", "^[\"](.*)[\"]$"])
+  classes.append([14, "abrir parênteses", "[(]"])
+  classes.append([15, "fechar parênteses", "[)]"])
+  classes.append([16, "abrir chaves", "[{]"])
+  classes.append([17, "fechar chaves", "[]}]"])
+  classes.append([18, "delimitador de linha", "[;]"])
 
-  return classes;
+  return classes
   
-
+#divide o código em lexemas
 def trataTextoCodigo(codigo):
-  #Remove os espaços seguidos
-  #codigo = codigo.replace("  ", " ")  
+  #Alterar qualquer tipo de espaço em branco(\t\n\r\f\v) por ' '
   codigo = re.sub("\s", " ", codigo)
+  
+  #Remove os espaços seguidos
   codigo = re.sub("[ ]+", " ", codigo)
 
   #Divide o codigo pelo ' '
   lexemas = codigo.split(" ")
 
   return lexemas
-#  for item in lexemas :
- #   item = '_' + item + '_'
-  #  print('Codigo é:', item)
- 
-  #Remove as tabulações
- # for item in lexemas :
-  #  item = item.replace("\t", "")
- 
-  #Remove os espaços
- # for item in lexemas :
-  #  item = item.replace(" ", "")    
-   # if item == "" or "\t" or "\n":
-    #  del(lexemas[lexemas.index(item)])
-
-  
-
-def reconhecerExpressaoRegular(fragmentoTexto):
-  result = true
-  for classe in definirClasses():
-    re.match fragmentoTexto
-
-def criaToken(fragmentoTexto):
-  s = 1
-    
 
 lexemas = []
 tokens = []
+tabelaSimbolos = []
+palavrasReservadas = definirPalavrasReservadas()
 
-palavrasReservadas = definirPalavrasReservadas();
 
 print('Insira o codigo\n')
-#codigo = trataTextoCodigo(inputCodigo())
 codigo = inputCodigo()
 lexemas = trataTextoCodigo(codigo)
 
+
 #Verifica se o lexema pertence a linguagem e depois cria o token
 for lexema in lexemas :
-  if(reconhecerExpressaoRegular(lexema)):
-    token.append(criaToken)
+  classes =  definirClasses()
+  for classe in classes:  
+    if re.match(classe[2], lexema):
+      if classe[0] != 7:
+        tokens.append([classe[0], lexema])
+      #verifica se o indentificador não é uma palavra reservada
+      elif lexema not in palavrasReservadas:        
+        if(lexema not in tabelaSimbolos):
+          tokens.append([classe[0], len(tabelaSimbolos)])
+          tabelaSimbolos.append(lexema)
+        else:
+          tokens.append([classe[0], tabelaSimbolos.index(lexema)])
 
- 
-#Imprime
-for item in lexemas :
- item = '_' + item + '_'
- print('Codigo é:', item)
+
+print('\nTokens gerados:\n')
+print('O caractere "," foi escolhido para, dentro de cada token, separar o código do tipo de lexema do lexema propriamente dito.')
+print('Para mais fácil visualização, cada token será representado em uma diferente linha.\n')
+
+#Imprime tokens
+for token in tokens:
+  if(token[0] == 7):
+    print(str(token[0]) + ',' + tabelaSimbolos[token[1]])
+  else:
+    print(str(token[0]) + ',' + token[1])
+
+print('\nTabelas de Simbolos gerada:\n')
+
+#Imprime tokens
+for simbolo in tabelaSimbolos:
+    print(simbolo)
